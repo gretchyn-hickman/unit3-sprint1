12967db3e012f5029f8da5a8a5e89f6ebb33d2b6
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-06-12 21:01:13 -0600

    fixing tests issues

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,139 +1,185 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
+
 const initialMessage = "";
 const initialEmail = "";
 const initialSteps = 0;
 const initialIndex = 4;
 
-export default function AppFunctional(props) {
-  const [index, setIndex] = useState(initialIndex);
-  const [email, setEmail] = useState(initialEmail);
-  const [steps, setSteps] = useState(initialSteps);
-  const [message, setMessage] = useState(initialMessage);
-  const [x, setX] = useState(2);
-  const [y, setY] = useState(2);
+const initialState = {
+  message: initialMessage,
+  email: initialEmail,
+  index: initialIndex,
+  steps: initialSteps,
+  x: 2,
+  y: 2,
+};
 
-  function reset() {
-    setIndex(initialIndex);
-    setEmail(initialEmail);
-    setSteps(initialSteps);
-    setX(2);
-    setY(2);
+export default class AppClass extends React.Component {
+  constructor() {
+    super();
+    this.state = { ...initialState };
   }
 
-  function getNextIndex(direction) {
+  reset = () => {
+    this.setState(initialState);
+  };
+
+  getNextIndex = (direction) => {
     if (direction === "reset") {
-      reset();
+      this.reset();
     }
     if (direction === "left") {
-      if (index % 3 !== 0) {
-        setIndex(index - 1);
-        setSteps(steps + 1);
-        setX(x - 1);
+      if (this.state.index % 3) {
+        this.setState({
+          ...this.state,
+          index: this.state.index - 1,
+          message: initialMessage,
+          steps: this.state.steps + 1,
+          x: this.state.x - 1,
+        });
       } else {
-        setMessage("You can't go left");
+        this.setState({
+          ...this.state,
+          message: "You can't go left",
+        });
       }
     }
     if (direction === "right") {
-      if ((index + 1) % 3 !== 0) {
-        setIndex(index + 1);
-        setSteps(steps + 1);
-        setX(x + 1);
+      if ((this.state.index + 1) % 3) {
+        this.setState({
+          ...this.state,
+          index: this.state.index + 1,
+          message: initialMessage,
+          steps: this.state.steps + 1,
+          x: this.state.x + 1,
+        });
       } else {
-        setMessage("You can't go right");
+        this.setState({
+          ...this.state,
+          message: "You can't go right",
+        });
       }
     }
     if (direction === "up") {
-      if (!(index < 3)) {
-        setIndex(index - 3);
-        setSteps(steps + 1);
-        setY(y - 1);
+      if (this.state.index > 2) {
+        this.setState({
+          ...this.state,
+          index: this.state.index - 3,
+          message: initialMessage,
+          steps: this.state.steps + 1,
+          y: this.state.y - 1,
+        });
       } else {
-        setMessage("You can't go up");
+        this.setState({
+          ...this.state,
+          message: "You can't go up",
+        });
       }
     }
     if (direction === "down") {
-      if (index <= 5) {
-        setIndex(index + 3);
-        setSteps(steps + 1);
-        setY(y + 1);
+      if (this.state.index < 6) {
+        this.setState({
+          ...this.state,
+          index: this.state.index + 3,
+          message: initialMessage,
+          steps: this.state.steps + 1,
+          y: this.state.y + 1,
+        });
       } else {
-        setMessage("You can't go down");
+        this.setState({
+          ...this.state,
+          message: "You can't go down",
+        });
       }
     }
-  }
+  };
 
-  function move(evt) {
-    setMessage("");
-    getNextIndex(evt.target.id);
-  }
+  move = (evt) => {
+    this.getNextIndex(evt.target.id);
+  };
 
-  function onChange(evt) {
-    setEmail(evt.target.value);
-  }
+  onChange = (evt) => {
+    this.setState({
+      ...this.state,
+      email: evt.target.value,
+    });
+  };
 
-  function onSubmit(evt) {
+  onSubmit = (evt) => {
     evt.preventDefault();
     axios
-      .post("http://localhost:9000/api/result", {
-        x: x,
-        y: y,
-        steps: steps,
-        email: email,
+      .post(`http://localhost:9000/api/result`, {
+        x: this.state.x,
+        y: this.state.y,
+        steps: this.state.steps,
+        email: this.state.email,
       })
-      .then((res) => setMessage(res.data.message))
-      .catch((err) => setMessage(err.response.data.message));
+      .then((res) =>
+        this.setState({
+          ...this.state,
+          message: res.data.message,
+          email: initialEmail,
+        })
+      )
+      .catch((err) =>
+        this.setState({ ...this.state, message: err.response.data.message })
+      );
+  };
 
-    setEmail(initialEmail);
+  render() {
+    const { className } = this.props;
+    return (
+      <div id="wrapper" className={className}>
+        <div className="info">
+          <h3 id="coordinates">
+            Coordinates ({this.state.x}, {this.state.y})
+          </h3>
+          <h3 id="steps">
+            You moved {this.state.steps} time{this.state.steps !== 1 && "s"}
+          </h3>
+        </div>
+        <div id="grid">
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+            <div
+              key={idx}
+              className={`square${idx === this.state.index ? " active" : ""}`}
+            >
+              {idx === this.state.index ? "B" : null}
+            </div>
+          ))}
+        </div>
+        <div className="info">
+          <h3 id="message">{this.state.message}</h3>
+        </div>
+        <div id="keypad">
+          <button id="left" onClick={this.move}>
+            LEFT
+          </button>
+          <button id="up" onClick={this.move}>
+            UP
+          </button>
+          <button id="right" onClick={this.move}>
+            RIGHT
+          </button>
+          <button id="down" onClick={this.move}>
+            DOWN
+          </button>
+          <button id="reset" onClick={this.move}>
+            reset
+          </button>
+        </div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            id="email"
+            type="email"
+            placeholder="type email"
+            onChange={this.onChange}
+            value={this.state.email}
+          ></input>
+          <input id="submit" type="submit"></input>
+        </form>
+      </div>
+    );
   }
-
-  return (
-    <div id="wrapper" className={props.className}>
-      <div className="info">
-        <h3 id="coordinates">
-          Coordinates ({x}, {y})
-        </h3>
-        <h3 id="steps">
-          {steps !== 1 ? `You moved ${steps} times` : `You moved ${steps} time`}
-        </h3>
-      </div>
-      <div id="grid">
-        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
-          <div key={idx} className={`square${idx === index ? " active" : ""}`}>
-            {idx === index ? "B" : null}
-          </div>
-        ))}
-      </div>
-      <div className="info">
-        <h3 id="message">{message}</h3>
-      </div>
-      <div id="keypad">
-        <button id="left" onClick={move}>
-          LEFT
-        </button>
-        <button id="up" onClick={move}>
-          UP
-        </button>
-        <button id="right" onClick={move}>
-          RIGHT
-        </button>
-        <button id="down" onClick={move}>
-          DOWN
-        </button>
-        <button id="reset" onClick={move}>
-          reset
-        </button>
-      </div>
-      <form onSubmit={onSubmit}>
-        <input
-          id="email"
-          type="email"
-          placeholder="type email"
-          onChange={onChange}
-          value={email}
-        ></input>
-        <input id="submit" type="submit"></input>
-      </form>
-    </div>
-  );
 }
